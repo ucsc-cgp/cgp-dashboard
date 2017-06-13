11903d4140ec6e5ce3b25baed341e422605d4258
caaespin <caaespin@ucsc.edu>
2017-06-13 00:30:17 +0000

    Adding the plain ES query in comments

--- app.py
+++ app.py
@@ -108,7 +108,33 @@ def get_google_auth(state=None, token=None):
     return oauth
 
 def query_es():
-    
+    """
+GET burn_idx/_search
+{
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "regexp": {
+            "experimentalStrategy": "[rR][nN][aA][-][Ss][Ee][Qq]"
+          }
+        },{
+          "regexp":{
+            "software": "[Ss]pinnaker"
+          }
+        }
+      ]
+    }
+  },
+  "aggs": {
+    "filtered_jobs":{
+      "cardinality": {
+        "field": "repoDataBundleId"
+      }
+    }
+  }
+}
+    """
     pass
 
 @app.route('/')
