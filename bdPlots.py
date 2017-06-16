832e936125540f194a4739ad53ec4f98afee5d8c
caaespin <caaespin@ucsc.edu>
2017-06-16 00:28:20 +0000

    Fixes now puts entries into the db. Now I need to remove entries that are older

--- bdPlots.py
+++ bdPlots.py
@@ -4,8 +4,9 @@ from app import query_es_rna_seq
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 from models import Burndown
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
-from sqlalchemy.orm import session
 
 es_service = os.environ.get("ES_SERVICE", "localhost")
 es = Elasticsearch(['http://'+es_service+':9200/'])
@@ -20,6 +21,14 @@ finished_jobs = query_es_rna_seq(es, 'burn_idx', myQuery, "repoDataBundleId")
 # Create the plot point
 plot = Burndown(total_jobs=total_jobs, finished_jobs=finished_jobs)
 # Enter the plot point in the table
+bd_user = os.getenv('BD_POSTGRES_USER')
+bd_password = os.getenv('BD_POSTGRES_PASSWORD')
+bd_table = os.getenv('BD_POSTGRES_DB')
+db_url = 'postgresql://{}:{}@bdchart-db/{}'.format(bd_user, bd_password,
+                                                   bd_table)
+engine = create_engine(db_url)
+session = sessionmaker()
+session.configure(bind=engine)
 s = session()
 s.add(plot)
 s.commit() 
