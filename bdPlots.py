b849bb06597899deecccec999612c2e2c4fcef4a
caaespin <caaespin@ucsc.edu>
2017-06-17 00:26:01 +0000

    Adding method for returning all the Burndown objects in the databse

--- bdPlots.py
+++ bdPlots.py
@@ -3,7 +3,7 @@
 from app import query_es_rna_seq
 from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch
-from models import Burndown
+from models import Burndown, get_all
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
@@ -36,6 +36,6 @@ old_entries = datetime.today().replace(second=0, microsecond=0) - timedelta(minu
 s.query(Burndown).filter(Burndown.captured_date <= old_entries).delete()
 # Commit Changes to the DB
 s.commit()
-print [(str(x.total_jobs), x.captured_date) for x in s.query(Burndown).all()]
+print [(str(x.total_jobs), x.captured_date) for x in get_all()]
 print "Total Jobs: {} Finished Jobs: {}".format(total_jobs, finished_jobs)
 print "Hello World"
--- models.py
+++ models.py
@@ -28,3 +28,17 @@ def initialize_table():
     session = sessionmaker()
     session.configure(bind=engine)
     Base.metadata.create_all(engine)
+
+
+def get_all():
+    bd_user = os.getenv('BD_POSTGRES_USER')
+    bd_password = os.getenv('BD_POSTGRES_PASSWORD')
+    bd_table = os.getenv('BD_POSTGRES_DB')
+    db_url = 'postgresql://{}:{}@bdchart-db/{}'.format(bd_user, bd_password,
+                                                       bd_table)
+    engine = create_engine(db_url)
+    session = sessionmaker()
+    session.configure(bind=engine)
+    s = session()
+    q = s.query(Burndown).order_by(Burndown.captured_date.desc()).all()
+    return q
