a6218eb8a1732c9a3aecbf75ece22827b2a94edf
caaespin <caaespin@ucsc.edu>
2017-06-19 17:30:32 +0000

    Javascript code

--- static/burndown_chart/burndown.app.js
+++ static/burndown_chart/burndown.app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var timeStamps = captured_dates
-var allJobs = total_jobs
-var completedJobs = finished_jobs
+var timeStamps = captured_dates;
+var allJobs = total_jobs;
+var completedJobs = finished_jobs;
 
 var BurndownAPI_Connector = angular.module('BurndownAPI_Connector', ["chart.js"], function($interpolateProvider){
    $interpolateProvider.startSymbol('{-');
@@ -10,5 +10,10 @@ var BurndownAPI_Connector = angular.module('BurndownAPI_Connector', ["chart.js"]
 });
 
 BillingAPI_Connector.controller('burndown_controller', function($scope) {
-
+   $scope.labels = timeStamps;
+   $scope.series = ['All Jobs', 'Finished Jobs'];
+   $scope.data = [allJobs, completedJobs];
+   $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+   }
 })
