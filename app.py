b7fa157a593ad1879e3c8d63b1d7f8d04be70c0a
Charles Overbeck <coverbec@ucsc.edu>
2018-03-26 14:46:54 -0700

    Make email primary id

--- app.py
+++ app.py
@@ -91,18 +91,20 @@ login_manager.session_protection = "strong"
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), primary_key=True)
     name = db.Column(db.String(100), nullable=True)
     avatar = db.Column(db.String(200))
     access_token = db.Column(db.String(5000))
     tokens = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
+    def get_id(self):
+        return self.email
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.query.get(user_id)
 
 
 """ OAuth Session creation """
