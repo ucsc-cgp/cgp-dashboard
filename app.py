a899b732669b764f2ee3c89dc5ce54715baa6c49
Michael Krause <mkrause1@ucsc.edu>
2018-12-01 11:35:48 -0800

    Added env var to app config to refresh cookie w/e request

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ class Config:
     # Make cookies secure so that the tokens stored in them are safe and only travel over https
     SESSION_COOKIE_SECURE = True
     REMEMBER_COOKIE_SECURE = True
+    REMEMBER_COOKIE_REFRESH_EACH_REQUEST = True
 
 
 class DevConfig(Config):
